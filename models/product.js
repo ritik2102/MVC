d79c7da3a82367f4e547ecb8cfc7b084a3bb16d1
ritik2102 <ritik21feb@gmail.com>
2023-03-25 21:45:39 +0530

    Working with MVC

---
+++ models/product.js
@@ -0,0 +1,68 @@
+// model for an individual product
+const fs=require('fs');
+const path=require('path');
+const rootDir=require('../util/path');
+const p=path.join(rootDir,'data','products.json');
+
+const getProductsFromFile=(cb)=>{
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            cb([]);
+        }
+        else{
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
+
+module.exports = class Product{
+    constructor(title){
+        this.title=title;
+    }
+
+    read(){
+        fs.readFile(p,(err,fileContent)=>{
+            if(!err){
+                return JSON.parse(fileContent);
+            }
+            else{
+                return [];
+            }
+        });
+    }
+    save(){
+
+        getProductsFromFile(products=>{
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products),err=>{
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(cb){
+        getProductsFromFile(cb);
+    }
+}
+
+// save(){
+//     let products=[];
+//     fs.readFile(p,(err,fileContent)=>{
+//         if(!err){
+//             products=JSON.parse(fileContent);
+//         }
+//         products.push(this);
+//         fs.writeFile(p,JSON.stringify(products),(err)=>{
+//             console.log(err);
+//         });
+//     });
+// }
+
+// static fetchAll(cb){
+//     fs.readFile(p,(err,fileContent)=>{
+//         if(err){
+//             cb([]);
+//         }
+//         cb(JSON.parse(fileContent));
+//     });
+// }
